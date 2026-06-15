a589e3a5388fe2874e1454022b9bd54c65e1aa2f
André IKOTO <126103789+ikoto92@users.noreply.github.com>
2026-06-15 13:50:21 +0200

    Update Equipement.cs

--- RGP/Equipement.cs
+++ RGP/Equipement.cs
@@ -13,14 +13,12 @@ namespace RGP
         private int bonusForce;
         private int bonusAgilite;
         private int bonusIntelligence;
-
         public string Nom { get; set; }
         public int Endurance { get; set; }
         public int Force { get; set; }
         public int Agilite { get; set; }
         public int Intelligence { get; set; }
         public int CoutPA { get; set; }
-
         public Equipement(string nom, int bonusEndurance, int bonusForce, int bonusAgilite, int bonusIntelligence)
         {
             this.nom = nom;
@@ -29,22 +27,18 @@ namespace RGP
             this.bonusAgilite = bonusAgilite;
             this.bonusIntelligence = bonusIntelligence;
         }
-
         public int BonusEndurance
         {
             get { return bonusEndurance; }
         }
-
         public int BonusForce
         {
             get { return bonusForce; }
         }
-
         public int BonusAgilite
         {
             get { return bonusAgilite; }
         }
-
         public int BonusIntelligence
         {
             get { return bonusIntelligence; }
